7083fb38b233f8b152d9903175d0860c15e103f1
mvinothanand <vinoth.anand@gmail.com>
2025-12-10 13:42:03 +0530

    Keeping react components pure.

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import ProductList from './ProductList';
 import Counter from './Counter';
 import Quiz from './Quiz';
 import QuizDemo from './QuizDemo';
+// import BootstrapNavbar from './BootstrapNavbar';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import ImpureComponent from './ImpureComponent';
+import PureComponent from './PureComponent';
 
 function App() {
 
   return (
-    <div className='grid grid-cols-1 gap-2 w-full bg-slate-100 justify-center items-center'>
-      {/* <ProductList/>
-      <Counter/> */}
-      <Quiz/>
-      <QuizDemo/>
+    <div className='grid grid-cols-2 gap-2 border-2'>
+      <ImpureComponent/>
+      {/* <hr></hr>
+      <PureComponent/> */}
     </div>
   )
 }
---
+++ src/ImpureComponent.jsx
@@ -0,0 +1,18 @@
+let guest = 0;
+
+function Cup() {
+  // Bad: changing a preexisting variable
+  guest = guest + 1;
+  console.log(`Guest: ${guest}`);
+  return <h2>Tea cup for guest #{guest}</h2>;
+}
+
+export default function TeaSet() {
+  return (
+    <>
+      <Cup />
+      <Cup />
+      <Cup />
+    </>
+  );
+}
